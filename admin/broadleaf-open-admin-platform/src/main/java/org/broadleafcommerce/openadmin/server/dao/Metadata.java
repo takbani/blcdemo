3c9c99fef263c850fe1658bfdf10938352b0232c
Taiz Akbani <t.akbani@gmail.com>
2015-06-21 11:14:51 +0100

    Initial Commit

---
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/dao/Metadata.java
@@ -0,0 +1,205 @@
+/*
+ * #%L
+ * BroadleafCommerce Open Admin Platform
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.broadleafcommerce.openadmin.server.dao;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.broadleafcommerce.common.presentation.AdminPresentationClass;
+import org.broadleafcommerce.common.presentation.PopulateToOneFieldsEnum;
+import org.broadleafcommerce.common.presentation.client.SupportedFieldType;
+import org.broadleafcommerce.openadmin.dto.FieldMetadata;
+import org.broadleafcommerce.openadmin.dto.MergedPropertyType;
+import org.broadleafcommerce.openadmin.server.dao.provider.metadata.DefaultFieldMetadataProvider;
+import org.broadleafcommerce.openadmin.server.dao.provider.metadata.FieldMetadataProvider;
+import org.broadleafcommerce.openadmin.server.dao.provider.metadata.request.AddMetadataFromMappingDataRequest;
+import org.broadleafcommerce.openadmin.server.dao.provider.metadata.request.AddMetadataRequest;
+import org.broadleafcommerce.openadmin.server.dao.provider.metadata.request.OverrideViaAnnotationRequest;
+import org.broadleafcommerce.openadmin.server.dao.provider.metadata.request.OverrideViaXmlRequest;
+import org.broadleafcommerce.openadmin.server.service.type.FieldProviderResponse;
+import org.hibernate.mapping.Property;
+import org.hibernate.type.Type;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jeff Fischer
+ */
+@Component("blMetadata")
+@Scope("prototype")
+public class Metadata {
+
+    private static final Log LOG = LogFactory.getLog(Metadata.class);
+
+    @Resource(name="blMetadataProviders")
+    protected List<FieldMetadataProvider> fieldMetadataProviders = new ArrayList<FieldMetadataProvider>();
+
+    @Resource(name= "blDefaultFieldMetadataProvider")
+    protected FieldMetadataProvider defaultFieldMetadataProvider;
+
+    public Map<String, FieldMetadata> getFieldPresentationAttributes(Class<?> parentClass, Class<?> targetClass, DynamicEntityDao dynamicEntityDao, String prefix) {
+        Map<String, FieldMetadata> attributes = new HashMap<String, FieldMetadata>();
+        Field[] fields = dynamicEntityDao.getAllFields(targetClass);
+        for (Field field : fields) {
+            boolean foundOneOrMoreHandlers = false;
+            for (FieldMetadataProvider fieldMetadataProvider : fieldMetadataProviders) {
+                FieldProviderResponse response = fieldMetadataProvider.addMetadata(new AddMetadataRequest(field, parentClass, targetClass,
+                        dynamicEntityDao, prefix), attributes);
+                if (FieldProviderResponse.NOT_HANDLED != response) {
+                    foundOneOrMoreHandlers = true;
+                }
+                if (FieldProviderResponse.HANDLED_BREAK == response) {
+                    break;
+                }
+            }
+            if (!foundOneOrMoreHandlers) {
+                defaultFieldMetadataProvider.addMetadata(new AddMetadataRequest(field, parentClass, targetClass,
+                        dynamicEntityDao, prefix), attributes);
+            }
+        }
+        return attributes;
+    }
+
+    public Map<String, FieldMetadata> overrideMetadata(Class<?>[] entities, PropertyBuilder propertyBuilder, String prefix, Boolean isParentExcluded, String ceilingEntityFullyQualifiedClassname, String configurationKey, DynamicEntityDao dynamicEntityDao) {
+        Boolean classAnnotatedPopulateManyToOneFields = null;
+        //go in reverse order since I want the lowest subclass override to come last to guarantee that it takes effect
+        for (int i = entities.length-1;i >= 0; i--) {
+            AdminPresentationClass adminPresentationClass = entities[i].getAnnotation(AdminPresentationClass.class);
+            if (adminPresentationClass != null && adminPresentationClass.populateToOneFields() != PopulateToOneFieldsEnum.NOT_SPECIFIED) {
+                classAnnotatedPopulateManyToOneFields = adminPresentationClass.populateToOneFields()==PopulateToOneFieldsEnum.TRUE;
+                break;
+            }
+        }
+
+        Map<String, FieldMetadata> mergedProperties = propertyBuilder.execute(classAnnotatedPopulateManyToOneFields);
+        for (int i = entities.length-1;i >= 0; i--) {
+            boolean handled = false;
+            for (FieldMetadataProvider fieldMetadataProvider : fieldMetadataProviders) {
+                FieldProviderResponse response = fieldMetadataProvider.overrideViaAnnotation(new OverrideViaAnnotationRequest(entities[i],
+                            isParentExcluded, dynamicEntityDao, prefix), mergedProperties);
+                if (FieldProviderResponse.NOT_HANDLED != response) {
+                    handled = true;
+                }
+                if (FieldProviderResponse.HANDLED_BREAK == response) {
+                    break;
+                }
+            }
+            if (!handled) {
+                defaultFieldMetadataProvider.overrideViaAnnotation(new OverrideViaAnnotationRequest(entities[i],
+                                         isParentExcluded, dynamicEntityDao, prefix), mergedProperties);
+            }
+        }
+        ((DefaultFieldMetadataProvider) defaultFieldMetadataProvider).overrideExclusionsFromXml(new OverrideViaXmlRequest(configurationKey,
+                ceilingEntityFullyQualifiedClassname, prefix, isParentExcluded, dynamicEntityDao), mergedProperties);
+
+        boolean handled = false;
+        for (FieldMetadataProvider fieldMetadataProvider : fieldMetadataProviders) {
+            FieldProviderResponse response = fieldMetadataProvider.overrideViaXml(
+                    new OverrideViaXmlRequest(configurationKey, ceilingEntityFullyQualifiedClassname, prefix,
+                            isParentExcluded, dynamicEntityDao), mergedProperties);
+            if (FieldProviderResponse.NOT_HANDLED != response) {
+                handled = true;
+            }
+            if (FieldProviderResponse.HANDLED_BREAK == response) {
+                break;
+            }
+        }
+        if (!handled) {
+            defaultFieldMetadataProvider.overrideViaXml(
+                                new OverrideViaXmlRequest(configurationKey, ceilingEntityFullyQualifiedClassname, prefix,
+                                        isParentExcluded, dynamicEntityDao), mergedProperties);
+        }
+
+        return mergedProperties;
+    }
+
+    public FieldMetadata getFieldMetadata(
+        String prefix,
+        String propertyName,
+        List<Property> componentProperties,
+        SupportedFieldType type,
+        Type entityType,
+        Class<?> targetClass,
+        FieldMetadata presentationAttribute,
+        MergedPropertyType mergedPropertyType,
+        DynamicEntityDao dynamicEntityDao
+    ) {
+        return getFieldMetadata(prefix, propertyName, componentProperties, type, null, entityType, targetClass, presentationAttribute, mergedPropertyType, dynamicEntityDao);
+    }
+
+    public FieldMetadata getFieldMetadata(
+        String prefix,
+        final String propertyName,
+        final List<Property> componentProperties,
+        final SupportedFieldType type,
+        final SupportedFieldType secondaryType,
+        final Type entityType,
+        Class<?> targetClass,
+        final FieldMetadata presentationAttribute,
+        final MergedPropertyType mergedPropertyType,
+        final DynamicEntityDao dynamicEntityDao
+    ) {
+        if (presentationAttribute.getTargetClass() == null) {
+            presentationAttribute.setTargetClass(targetClass.getName());
+            presentationAttribute.setFieldName(propertyName);
+        }
+        presentationAttribute.setInheritedFromType(targetClass.getName());
+        presentationAttribute.setAvailableToTypes(new String[]{targetClass.getName()});
+        boolean handled = false;
+        for (FieldMetadataProvider fieldMetadataProvider : fieldMetadataProviders) {
+            FieldProviderResponse response = fieldMetadataProvider.addMetadataFromMappingData(new AddMetadataFromMappingDataRequest(
+                componentProperties, type, secondaryType, entityType, propertyName, mergedPropertyType, dynamicEntityDao), presentationAttribute);
+            if (FieldProviderResponse.NOT_HANDLED != response) {
+                handled = true;
+            }
+            if (FieldProviderResponse.HANDLED_BREAK == response) {
+                break;
+            }
+        }
+        if (!handled) {
+            defaultFieldMetadataProvider.addMetadataFromMappingData(new AddMetadataFromMappingDataRequest(
+                    componentProperties, type, secondaryType, entityType, propertyName, mergedPropertyType, dynamicEntityDao), presentationAttribute);
+        }
+
+        return presentationAttribute;
+    }
+
+    public FieldMetadataProvider getDefaultFieldMetadataProvider() {
+        return defaultFieldMetadataProvider;
+    }
+
+    public void setDefaultFieldMetadataProvider(FieldMetadataProvider defaultFieldMetadataProvider) {
+        this.defaultFieldMetadataProvider = defaultFieldMetadataProvider;
+    }
+
+    public List<FieldMetadataProvider> getFieldMetadataProviders() {
+        return fieldMetadataProviders;
+    }
+
+    public void setFieldMetadataProviders(List<FieldMetadataProvider> fieldMetadataProviders) {
+        this.fieldMetadataProviders = fieldMetadataProviders;
+    }
+}
---
+++ admin/broadleaf-open-admin-platform/src/main/java/org/broadleafcommerce/openadmin/server/util/MetadataOverrideBeanDefinitionParser.java
@@ -0,0 +1,168 @@
+/*
+ * #%L
+ * BroadleafCommerce Open Admin Platform
+ * %%
+ * Copyright (C) 2009 - 2013 Broadleaf Commerce
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.broadleafcommerce.openadmin.server.util;
+
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.broadleafcommerce.openadmin.dto.override.FieldMetadataOverride;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.MapFactoryBean;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.ManagedMap;
+import org.springframework.beans.factory.xml.AbstractBeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.springframework.util.xml.DomUtils;
+import org.w3c.dom.Element;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jeff Fischer
+ */
+public class MetadataOverrideBeanDefinitionParser extends AbstractBeanDefinitionParser {
+
+    @Override
+    protected AbstractBeanDefinition parseInternal(Element element, ParserContext parserContext) {
+        BeanDefinitionBuilder overallMapBuilder = BeanDefinitionBuilder.rootBeanDefinition(MapFactoryBean.class);
+        Map<String, BeanDefinition> overallMap = new ManagedMap<String, BeanDefinition>();
+        List<Element> overrideItemElements = DomUtils.getChildElementsByTagName(element, "overrideItem");
+        for (Element overrideItem : overrideItemElements) {
+            String configKey = overrideItem.getAttribute("configurationKey");
+            String ceilingEntity = overrideItem.getAttribute("ceilingEntity");
+
+            if (StringUtils.isEmpty(configKey) && StringUtils.isEmpty(ceilingEntity)) {
+                throw new IllegalArgumentException("Must specify either a configurationKey or a ceilingEntity attribute for the overrideItem element");
+            }
+
+            BeanDefinitionBuilder fieldMapBuilder = BeanDefinitionBuilder.rootBeanDefinition(MapFactoryBean.class);
+            Map<String, BeanDefinition> fieldMap = new ManagedMap<String, BeanDefinition>();
+            List<Element> fieldElements = DomUtils.getChildElementsByTagName(overrideItem, "field");
+            for (Element fieldElement : fieldElements) {
+                String fieldName = fieldElement.getAttribute("name");
+                BeanDefinitionBuilder metadataBuilder = BeanDefinitionBuilder.rootBeanDefinition(FieldMetadataOverride.class);
+                fieldMap.put(fieldName, metadataBuilder.getBeanDefinition());
+                {
+                    List<Element> propElements = DomUtils.getChildElementsByTagName(fieldElement, "property");
+                    for (Element propElement : propElements) {
+                        String propName = propElement.getAttribute("name");
+                        String propValue = propElement.getAttribute("value");
+                        metadataBuilder.addPropertyValue(propName, propValue);
+                    }
+                }
+
+                {
+                    List<Element> validationElements = DomUtils.getChildElementsByTagName(fieldElement, "validation");
+                    Map<String, Map<String, String>> validationConfigMap = new ManagedMap<String, Map<String, String>>();
+                    for (Element validationElement : validationElements) {
+                        Map<String, String> validationMap = new ManagedMap<String, String>();
+                        List<Element> valPropElements = DomUtils.getChildElementsByTagName(validationElement, "property");
+                        for (Element valPropElement : valPropElements) {
+                            String valPropName = valPropElement.getAttribute("name");
+                            String valPropValue = valPropElement.getAttribute("value");
+                            validationMap.put(valPropName, valPropValue);
+                        }
+                        String className = validationElement.getAttribute("className");
+                        validationConfigMap.put(className, validationMap);
+                    }
+                    if (!validationConfigMap.isEmpty()) {
+                        metadataBuilder.addPropertyValue("validationConfigurations", validationConfigMap);
+                    }
+                }
+
+                {
+                    List<Element> optionElements = DomUtils.getChildElementsByTagName(fieldElement, "optionFilterValue");
+                    String[][] optionFilterValues = new String[optionElements.size()][3];
+                    int j = 0;
+                    for (Element optionElement : optionElements) {
+                        optionFilterValues[j][0] = optionElement.getAttribute("name");
+                        optionFilterValues[j][1] = optionElement.getAttribute("value");
+                        optionFilterValues[j][2] = optionElement.getAttribute("type");
+                        j++;
+                    }
+                    if (!ArrayUtils.isEmpty(optionFilterValues)) {
+                        metadataBuilder.addPropertyValue("optionFilterValues", optionFilterValues);
+                    }
+                }
+
+                {
+                    List<Element> keyElements = DomUtils.getChildElementsByTagName(fieldElement, "mapKey");
+                    String[][] keyValues = new String[keyElements.size()][2];
+                    int j = 0;
+                    for (Element keyElement : keyElements) {
+                        keyValues[j][0] = keyElement.getAttribute("value");
+                        keyValues[j][1] = keyElement.getAttribute("displayValue");
+                        j++;
+                    }
+                    if (!ArrayUtils.isEmpty(keyValues)) {
+                        metadataBuilder.addPropertyValue("keys", keyValues);
+                    }
+                }
+
+                {
+                    List<Element> children = DomUtils.getChildElementsByTagName(fieldElement, "customCriteria");
+                    String[] values = new String[children.size()];
+                    int j = 0;
+                    for (Element childElem : children) {
+                        values[j] = childElem.getAttribute("value");
+                        j++;
+                    }
+                    if (!ArrayUtils.isEmpty(values)) {
+                        metadataBuilder.addPropertyValue("customCriteria", values);
+                    }
+                }
+
+                {
+                    List<Element> children = DomUtils.getChildElementsByTagName(fieldElement, "maintainedAdornedTargetField");
+                    String[] values = new String[children.size()];
+                    int j = 0;
+                    for (Element childElem : children) {
+                        values[j] = childElem.getAttribute("value");
+                        j++;
+                    }
+                    if (!ArrayUtils.isEmpty(values)) {
+                        metadataBuilder.addPropertyValue("maintainedAdornedTargetFields", values);
+                    }
+                }
+
+                {
+                    List<Element> children = DomUtils.getChildElementsByTagName(fieldElement, "gridVisibleField");
+                    String[] values = new String[children.size()];
+                    int j = 0;
+                    for (Element childElem : children) {
+                        values[j] = childElem.getAttribute("value");
+                        j++;
+                    }
+                    if (!ArrayUtils.isEmpty(values)) {
+                        metadataBuilder.addPropertyValue("gridVisibleFields", values);
+                    }
+                }
+            }
+            fieldMapBuilder.addPropertyValue("sourceMap", fieldMap);
+
+            overallMap.put(StringUtils.isEmpty(configKey)?ceilingEntity:configKey, fieldMapBuilder.getBeanDefinition());
+        }
+
+        overallMapBuilder.addPropertyValue("sourceMap", overallMap);
+
+        return overallMapBuilder.getBeanDefinition();
+    }
+}
